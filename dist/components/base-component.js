2f388045aa283b55305f91fda423cfd7552eba58
Marcin Szaro <marcinszaro@gmail.com>
2023-06-26 14:46:01 +0200

    After section 10

---
+++ dist/components/base-component.js
@@ -0,0 +1,15 @@
+export class Component {
+    constructor(templateID, hostElementId, insertAtStart, newElementId) {
+        this.templateElement = document.getElementById(templateID);
+        this.hostElement = document.getElementById(hostElementId);
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild;
+        if (newElementId)
+            this.element.id = newElementId;
+        this.attach(insertAtStart);
+    }
+    attach(insertAtStart) {
+        this.hostElement.insertAdjacentElement(insertAtStart ? "afterbegin" : "beforeend", this.element);
+    }
+}
+//# sourceMappingURL=base-component.js.map
